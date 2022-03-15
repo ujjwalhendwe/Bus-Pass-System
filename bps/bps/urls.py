9534fe5bfa62114dc13fcd57a8477ad580de99c7
Harsh Verma <vharshverma18@gmail.com>
2022-03-15 23:26:11 +0530

    integrate

--- bps/bps/urls.py
+++ bps/bps/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
     path('accounts/', include('allauth.urls')),
+    path('hemlo/', TemplateView.as_view(template_name='account/temp.html')),
 ]
