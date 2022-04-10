14c664abdbddb56eb49ba24a18d1004a2f8f2240
Harsh Verma <vharshverma18@gmail.com>
2022-04-11 01:45:19 +0530

    backend

--- bps/bps/settings.py
+++ bps/bps/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'crispy_forms',
+    'bus'
 
 ]
 
--- bps/bps/urls.py
+++ bps/bps/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('home/', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
     path('accounts/', include('allauth.urls')),
     path('hemlo/', TemplateView.as_view(template_name='account/temp.html')),
+    path('tracking/', TemplateView.as_view(template_name='Trackbus.html')),
+    path('',include('bus.urls'))
 ]
